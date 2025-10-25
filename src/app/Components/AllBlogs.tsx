3f3e0e998d5f36cb3f80e7d87fe60c5f3a63c90e
Arun <arunjamdagni2@gmail.com>
2025-10-26 03:27:46 +0530

    changes

--- src/app/Components/AllBlogs.tsx
+++ src/app/Components/AllBlogs.tsx
@@ -42,8 +42,8 @@ const AllBlogs = () => {
     <section className="w-[100vw] relative min-h-screen  py-10  border-2 border-green-500">
       <div className="relative flex flex-col md:flex-row items-start justify-between gap px-4 md:px-10 lg:px-20 border-2 border-blue-500">
         {/* Main Content */}
-        <main className="flex-1 flex flex-col items-center justify-center w-full h-auto md:w-[70%]   border-2 border-red-500">
-          <div className="w-full h-full relative flex items-center justify-center flex-wrap flex-row gap-4">
+        <main className="flex-1 flex flex-col items-center justify-center w-full h-auto md:w-[70%] relative">
+          <div className="w-full h-full relative flex items-center justify-center flex-wrap flex-row gap-4 border-2 border-red-500">
            {validBlogs.length > 0 ? (
             validBlogs.map((blog: BlogsIncomingDTO) => (
               <BlogsCard key={blog.slug} {...blog} />
