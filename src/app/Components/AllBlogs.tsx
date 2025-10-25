4d7bd9ce4d0e161da5d757bc40f3c9af926a4335
Arun <arunjamdagni2@gmail.com>
2025-10-26 02:49:04 +0530

    changes

--- src/app/Components/AllBlogs.tsx
+++ src/app/Components/AllBlogs.tsx
@@ -39,8 +39,8 @@ const AllBlogs = () => {
     [];
 
   return (
-    <section className="w-full relative max-w-screen-xl mx-auto min-h-screen border-2 border-blue-400">
-      <div className="relative flex flex-col md:flex-row items-start justify-between gap-8 border-2 border-red-400 px-4 md:px-10 lg:px-20 py-10 sm:py-16">
+    <section className="w-full relative max-w-screen-xl mx-auto min-h-screen border-2 border-blue-400 px-4 md:px-10 lg:px-20 py-10 sm:py-16">
+      <div className="relative flex flex-col md:flex-row items-start justify-between gap-8 border-2 border-red-400 ">
         {/* Main Content */}
         <main className="flex-1 w-full overflow-y-scroll h-auto md:w-[70%] flex items-center justify-center flex-wrap flex-col sm:flex-row gap-4 border-2 border-yellow-400">
           {validBlogs.length > 0 ? (
