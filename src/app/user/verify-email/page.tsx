6f392b5bfe5738db851b31cc42f1a61e4715c756
Arun <arunjamdagni2@gmail.com>
2025-10-26 00:59:57 +0530

    changes

--- src/app/user/verify-email/page.tsx
+++ src/app/user/verify-email/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Spinner from '@/app/_components/Spinner';
 import authHook from '@/hooks/authHook';
 import React from 'react'
 
@@ -7,16 +8,16 @@ const VerifyEmailPage = () => {
     const {verifyUserEmail} = authHook();
      const urlParams = new URLSearchParams(window.location.search);
             const token = urlParams.get('token') || '';
-alert(token);
-    React.useEffect(()=>{
-        (async()=>{
+          React.useEffect(()=>{
+          (async()=>{
            
             await verifyUserEmail({token});
         })()
     },[token,verifyUserEmail])
 
   return (
-    <div className='max-w-screen w-full min-h-screen '>
+    <div className='max-w-screen w-full min-h-screen flex flex-col items-center justify-center gap-8'>
+        <Spinner/>
         <h2 className='text-center mt-20 text-2xl font-semibold'>Verifying your email...</h2>
 
     </div>
