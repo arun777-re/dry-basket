2aad3cac4ce1f6ad14819eb24a78bbb838b8a662
Arun <arunjamdagni2@gmail.com>
2025-10-26 02:02:06 +0530

    changes

--- src/app/Components/AllBlogs.tsx
+++ src/app/Components/AllBlogs.tsx
@@ -39,7 +39,7 @@ const AllBlogs = () => {
     [];
 
   return (
-    <section className="w-full max-w-screen-xl mx-auto min-h-screen px-4 sm:px-8 md:px-12 lg:px-20 py-10 sm:py-16 border-2 border-blue-400">
+    <section className="w-full max-w-screen-xl mx-auto min-h-screen px-4  border-2 border-blue-400">
       <div className="flex flex-col md:flex-row items-start justify-between gap-8 border-2 border-red-400">
         {/* Main Content */}
         <main className="flex-1 w-full md:w-[70%] flex items-center justify-center flex-wrap flex-row gap-4 border-2 border-yellow-400">
