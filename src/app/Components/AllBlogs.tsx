4a3b7284f91393b7193aaac159a554c5bfe94384
Arun <arunjamdagni2@gmail.com>
2025-10-26 03:10:19 +0530

    changes

--- src/app/Components/AllBlogs.tsx
+++ src/app/Components/AllBlogs.tsx
@@ -39,10 +39,10 @@ const AllBlogs = () => {
     [];
 
   return (
-    <section className="w-full relative max-w-screen-xl mx-auto min-h-scree px-4 md:px-10 lg:px-20 py-10">
-      <div className="relative flex flex-col md:flex-row items-start justify-between gap ">
+    <section className="w-full relative max-w-screen-xl mx-auto min-h-scree px-4 md:px-10 lg:px-20 py-10  border-2 border-green-500">
+      <div className="relative flex flex-col md:flex-row items-start justify-between gap  border-2 border-blue-500">
         {/* Main Content */}
-        <main className="flex-1 flex flex-col items-center justify-center w-full h-auto md:w-[70%]  ">
+        <main className="flex-1 flex flex-col items-center justify-center w-full h-auto md:w-[70%]   border-2 border-red-500">
           <div className="w-full h-full relative flex items-center justify-center flex-wrap flex-row gap-4">
            {validBlogs.length > 0 ? (
             validBlogs.map((blog: BlogsIncomingDTO) => (
@@ -66,7 +66,7 @@ const AllBlogs = () => {
         </main>
 
         {/* Sidebar */}
-        <aside className="w-full md:w-[30%] md:sticky md:top-24 space-y-6">
+        <aside className="w-full md:w-[30%] md:sticky md:top-24 space-y-6 border-2 border-green-500">
           {premiumProductData.slice(0, 2).map((item) => (
             <PremiumCard
               key={item.category}
